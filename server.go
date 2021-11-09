df914b05122a732e8bccb13a98244842b0e0bb68
Ondrej Sika <ondrej@ondrejsika.com>
2021-11-09 14:38:31 +0100

    feat: Add server.go

---
+++ server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	hostname, _ := os.Hostname()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World from Go! %s\n", hostname)
+		fmt.Println("GET " + r.URL.Path)
+	})
+
+	fmt.Println("Server started.")
+	http.ListenAndServe(":8000", nil)
+}
